490d9b149da79b10ff070a09324ddf227a7fc6fe
Jakob Weber <jakobweber@hotmail.com>
2020-04-14 23:13:12 +0200

    changed nothing

--- Code/telegram_msg_bot.py
+++ Code/telegram_msg_bot.py
@@ -5,8 +5,8 @@ import urllib
 def send_TG_msg(text):
     if text.find(" ") > 0:
         text = text.replace(" ", "+")
-    token = "1200420075:AAEqUONzhZEfBE6yPCGjWuftiHQANW0d_QI"
-    chatID = "-458410684"
-    url = "https://api.telegram.org/bot"+token+"/sendMessage?chat_id="+chatID+"&text="+text
+    token = "*1200420075:AAEqUONzhZEfBE6yPCGjWuftiHQANW0d_QI*"
+    chatID = "*-458410684*"
+    url = "https://api.telegram.org/bot"+token[1:-1]+"/sendMessage?chat_id="+chatID[1:-1]+"&text="+text
     # print(url)
     urllib.request.urlopen(url)
