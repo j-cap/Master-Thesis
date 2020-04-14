5c65675155dff5ab1df797a1f1fa5d63a312b4d0
Jakob Weber <jakobweber@hotmail.com>
2020-04-14 16:20:20 +0200

    Telegram message bot

---
+++ Code/TFL/tg_bot.py
@@ -0,0 +1,21 @@
+
+"""
+Telegram bot: 
+    -name: MyThesisBot
+    -token: 1200420075:AAEqUONzhZEfBE6yPCGjWuftiHQANW0d_QI
+    -chatID: -458410684
+
+"""
+
+
+import urllib
+
+def send_TG_msg(text):
+    if text.find(" ") > 0:
+        text = text.replace(" ", "+")
+    token = "1200420075:AAEqUONzhZEfBE6yPCGjWuftiHQANW0d_QI"
+    chatID = "-458410684"
+    url = "https://api.telegram.org/bot"+token+"/sendMessage?chat_id="+chatID+"&text="+text
+    # print(url)
+    urllib.request.urlopen(url)
+
