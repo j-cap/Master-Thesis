aabb88f0b75d7935fa0126ed073e6e933c018628
Jakob Weber <jakobweber@hotmail.com>
2020-06-15 15:35:57 +0200

    update TensorProductSplines

--- Code/SplinesInPython/TensorProductSplines.py
+++ Code/SplinesInPython/TensorProductSplines.py
@@ -3,19 +3,20 @@
 
 # **Tensor product spline implementation**
 
-# In[9]:
+# In[10]:
 
 
 # convert jupyter notebook to python script
 # get_ipython().system('jupyter nbconvert --to script TensorProductSplines.ipynb')
 
 
-# In[4]:
+# In[275]:
 
 
 import numpy as np
 import plotly.graph_objects as go
 
+from plotly.subplots import make_subplots
 from scipy.sparse import kron
 
 from ClassBSplines import BSpline
@@ -27,8 +28,8 @@ class TensorProductSpline(BSpline):
     
     def __init__(self, x1=None, x2=None):
         """It is important that len(x1) == len(x2)."""
-        self.x1 = x1
-        self.x2 = x2
+        self.x1 = np.unique(x1)
+        self.x2 = np.unique(x2)
         self.basis = None
         
     def tensor_product_spline_2d(self, k1=5, k2=5, print_shapes=False):
@@ -47,10 +48,23 @@ class TensorProductSpline(BSpline):
         BSpline_x2 = BSpline(self.x2)
         BSpline_x1.b_spline_basis(k=self.k1)
         BSpline_x2.b_spline_basis(k=self.k2)
+        
+        #BSpline_x1.plot_basis("1st B-Spline basis")
+        #BSpline_x2.plot_basis("2nd B-Spline basis")
+        
+        
         self.X1 = BSpline_x1.basis
         self.X2 = BSpline_x2.basis
-        self.basis = kron(self.X1, self.X2).toarray()
+        #self.basis = kron(self.X1, self.X2).toarray()
 
+        # kronecker product for TPS according to
+        # https://stats.stackexchange.com/questions/254542/surface-fit-using-tensor-product-of-b-splines 
+        X = np.zeros((self.x1.shape[0], self.X1.shape[1]*self.X2.shape[1]))
+        for i in range(X.shape[0]):
+            X[i,:] = np.kron(self.X1[i,:], self.X2[i,:])
+        self.basis = X
+        
+        
         if print_shapes:
             print("Shape of the first basis: ", self.X1.shape)
             print("Shape of the second basis: ", self.X2.shape)
@@ -67,7 +81,7 @@ class TensorProductSpline(BSpline):
             fig.add_trace(
                 go.Surface(
                     x=x1g, y=x2g,
-                    z=self.basis[:,i].reshape((self.X2.shape[0], self.X1.shape[0])),
+                    z=self.basis[:,i].reshape((self.x1.shape[0], self.x2.shape[0])),
                     name=f"TPS Basis {i+1}",
                     showscale=False
                 )
@@ -83,4 +97,80 @@ class TensorProductSpline(BSpline):
         )
         fig.show()
         return
+
+    
+    def plot_basis_individuel(self):
+
+        dim = self.basis.shape
+        dim_resh_1 = int(np.sqrt(dim[0]))
+        dim_resh_2 = int(np.sqrt(dim[1]))
+
+        
+        fig = make_subplots(rows=dim_resh_2, cols=dim_resh_2)
+
+        for i in range(dim[1]):
+            data = self.basis[:,i].reshape((dim_resh_1, dim_resh_1))
+            if i < dim_resh_2:
+                fig.add_trace(go.Heatmap(z=data), row=1, col=i+1)
+            elif i >= dim_resh_2 and i < 2*dim_resh_2:
+                fig.add_trace(go.Heatmap(z=data), row=2, col=i+1-dim_resh_2)
+            elif i >= 2*dim_resh_2 and i < 3*dim_resh_2:
+                fig.add_trace(go.Heatmap(z=data), row=3, col=i+1-2*dim_resh_2)
+            elif i >= 3*dim_resh_2 and i < 4*dim_resh_2:
+                fig.add_trace(go.Heatmap(z=data), row=4, col=i+1-3*dim_resh_2)
+            elif i >= 4*dim_resh_2:
+                fig.add_trace(go.Heatmap(z=data), row=5, col=i+1-4*dim_resh_2)
+
+        fig.update_traces(showscale=False)
+        fig.show()
+
+
+# In[352]:
+
+
+# Test for tensorproductspline
+
+#import plotly.graph_objects as go
+#import plotly.express as px
+#from plotly.subplots import make_subplots
+#from sklearn.metrics import mean_squared_error
+#
+#
+## data generation on a grid
+#samples = 250
+#x = np.linspace(-3, 3, samples)
+#xgrid = np.meshgrid(x,x)
+#grid = np.array([xgrid[1].ravel(), xgrid[0].ravel()]).T
+#
+## define test functions
+#def binorm(x,y):
+#    return (15/(2*np.pi)) * np.exp(-0.5 * (x**2 + y**2)) + 0.1*np.random.randn(x.shape[0])
+#
+#def xtimesy(x,y):
+#    return x * y  + np.random.randn(1)
+#
+#Y = binorm(grid[:,0], grid[:,1]).reshape((samples, samples))
+##Y = xtimesy(grid[:,0], grid[:,1]).reshape((samples, samples))
+#
+#go.Figure(go.Surface(z=Y, name="Test function")).show()
+#
+#T = TensorProductSpline(x1=np.unique(grid[:,0]), 
+#                        x2=np.unique(grid[:,1]))
+#
+#T.tensor_product_spline_2d(k1=12, k2=12)
+#
+## OLS
+#X = T.basis
+#beta = (np.linalg.pinv(X.T @ X) @ X.T @ Y)
+## prediction
+#pred = X @ beta
+#
+#fig = go.Figure()
+#fig = make_subplots(rows=1, cols=2,
+#                    specs=[[{"type": "scene"}, {"type": "scene"}]],)
+#fig.add_trace(go.Scatter3d(x=grid[:,0], y=grid[:,1], z=Y.ravel(), name="data", mode="markers"), row=1, col=1)
+#fig.add_trace(go.Scatter3d(x=grid[:,0], y=grid[:,1], z=pred.ravel(), name="pred"), row=1, col=1)
+#fig.show()
+#
+#mean_squared_error(pred.ravel(), Y.ravel())
 
