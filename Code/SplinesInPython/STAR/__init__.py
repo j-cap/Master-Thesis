5ad1624b52e86123da6473f52f8de255d1fe07d9
Jakob Weber <jakobweber@hotmail.com>
2020-09-02 16:08:16 +0200

    update

--- Code/SplinesInPython/STAR/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from .bspline import B_spline
-from .penalty_matrix import PenaltyMatrix
-from .smooth import Smooths
-from .star_model import StarModel
-from .tensorproductspline import TensorProductSpline
